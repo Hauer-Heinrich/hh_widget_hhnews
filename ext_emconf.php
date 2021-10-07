81e4de1424f517c773190237519087eca264b12a
Christian Hackl <hackl.chris@googlemail.com>
2021-10-07 10:43:43 +0200

    [TASK] change version to 1.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,7 +8,7 @@ $EM_CONF['hh_widget_hhnews'] = [
     'author_company' => 'Hauer-Heinrich.de',
     'state' => 'stable',
     'clearCacheOnLoad' => true,
-    'version' => '1.0.0',
+    'version' => '1.1.0',
     'autoload' => [
         'psr-4' => [
             'HauerHeinrich\\HhWidgetHhnews\\' => 'Classes'
@@ -16,7 +16,7 @@ $EM_CONF['hh_widget_hhnews'] = [
     ],
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
+            'typo3' => '10.4.0-11.5.99',
             'php' => '7.2.0-7.4.99',
             'dashboard' => ''
         ],
